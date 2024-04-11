a5a6c3f39e0f70d38ae6095fa63aa33ac0147a8d
Blaarkies <pierre.roux6@gmail.com>
2024-04-11 13:10:37 +0200

    add color cycler and animator

--- src/main.ts
+++ src/main.ts
@@ -1,96 +1,81 @@
-import { interval } from 'kefir';
-import { LED } from 'led';
 import {
-    hslToRgb,
-    makeNumberList,
-    sequencedInterval,
-    subject,
+    getHslAtKelvin,
+    MultiActionButton,
+    sum,
+    waitForDuration,
 } from './common';
-import { adcToCelsius } from './common/unit-of-measure';
-import { Ws2812 } from './devices';
+import { ColorCycler } from './state/color-selection';
+import { PixelsAnimator } from './state/pixels-animator';
+import { Ws2812 } from './ws2812/ws2812';
 
 
-// Display results in debug console
-let output = {
-    temperature: '-',
-    potentiometer: '-',
-};
+let ledPin = board.LED;
+pinMode(ledPin, OUTPUT);
 
-let update$ = subject();
-update$.throttle(500)
-    .onValue(() => console.log(`\n\n
-Temperature:   ${output.temperature}
-Potentiometer: ${output.potentiometer}
-`));
+let colorCycler = new ColorCycler(
+    {
+        warm: getHslAtKelvin(2000),
+        cold: getHslAtKelvin(4000),
+        red: [0, 1, .5],
+        green: [.33, 1, .5],
+        blue: [.67, 1, .5],
+    },
+);
 
+let pinButtonA = 0;
+let pinButtonB = 1;
+let pinPixels = 2;
 
-// Blink internal
-const internalLed = new LED(board.LED);
-let wink$ = sequencedInterval([100, 800, 300, 400, 100]);
-wink$.onValue(() => {
-    internalLed.toggle();
-});
-
-
-// Internal Temperature
-let tempAdc = board.adc(30);
-interval(1000, 0).onValue(() => {
-    let tempCelsius = adcToCelsius(tempAdc.read());
-    output.temperature = tempCelsius.toFixed(2) + ' °C';
-    update$.next();
+let pixels = new Ws2812(pinPixels, 74);
+let pixelsAnimator = new PixelsAnimator(
+    pixels,
+);
+pixelsAnimator.setColor(colorCycler.selectedRgb, {
+    animationType: 'sweep-center-out',
 });
 
 
 // Buttons
-let buttonAPin = 3;
-board.button(buttonAPin)
-    .addListener('click', () => {
-        console.log(`Button A was pressed!`);
-    });
+let buttonC = new MultiActionButton(pinButtonA);
+let isHeldC: boolean;
+buttonC.onHold({
+    callbackStart: () => isHeldC = true,
+    callbackEnd: () => isHeldC = false,
+});
 
-let buttonBPin = 4;
-board.button(buttonBPin)
-    .addListener('click', () => {
-        console.log(`Button B was pressed!`);
-    });
+let buttonB = new MultiActionButton(pinButtonB);
+let isHeldB: boolean;
+buttonB.onHold({
+    callbackStart: () => isHeldB = true,
+    callbackEnd: () => isHeldB = false,
+});
 
-
-// Potentiometer
-let potPin = 28;
-let potAdc = board.adc(potPin);
-let previousPotValue: number;
-interval(100, 0).onValue(() => {
-    let newValue = potAdc.read();
-    let threshold = .05;
-    let hasDifference = Math.abs(previousPotValue - newValue) > threshold;
-
-    // Blocks small changes (due to signal noise) from flooding the output
-    if (hasDifference) {
-        previousPotValue = newValue;
-        let barLength = 15;
-        let barString = makeNumberList(newValue * barLength)
-            .map(() => '#')
-            .join('')
-            .padEnd(barLength, '*');
-
-        output.potentiometer = barString;
-        update$.next();
+buttonC.onRelease(async () => {
+    if (isHeldB) {
+        colorCycler.cycleColor();
+        pixelsAnimator.setColor(colorCycler.selectedRgb);
+        return;
     }
+
+    colorCycler.toggle();
+
+    if (sum(colorCycler.selectedRgb) === 0) {
+        let handle = setInterval(() => digitalToggle(ledPin), 500);
+        await waitForDuration(2e3);
+        clearInterval(handle);
+        digitalWrite(ledPin, 0);
+    }
+
+    pixelsAnimator.setColor(colorCycler.selectedRgb);
+});
+
+buttonB.onRelease(() => {
+    colorCycler.cyclePower(isHeldC ? -1 : 1, 3);
+    pixelsAnimator.setColor(colorCycler.selectedRgb);
 });
 
 
-// WS2812 - Individually addressable led strip
-let pixelsPin = 2;
-let pixels = new Ws2812(pixelsPin, 2);
-interval(150, null)
-    .scan((acc, _) => acc + 1, -1)
-    .onValue(i => {
-        let resolution = 50;
-        let h1 = (i % resolution) / resolution;
-        let h2 = ((100 + i * 1.2) % resolution) / resolution;
 
-        pixels.setLedColor(0, hslToRgb(h1, 1, .5));
-        pixels.setLedColor(1, hslToRgb(h2, 1, .5));
 
-        pixels.write();
-    });
+
+
